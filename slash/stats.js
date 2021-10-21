cb860d2132f28b738a0360685bc519d4baf7f1b2
Artlfmj <arthurdepenanster@gmail.com>
2021-10-21 17:47:29 +0200

    Add stats

--- slash/stats.js
+++ slash/stats.js
@@ -2,6 +2,7 @@ const guild_settings = require('../schemes/guild_settings')
 const Settings = require('../schemes/guild_settings')
 const Discord = require('discord.js')
 const ee = require('../botconfig/embed.json')
+const axios = require('axios')
 module.exports = {
 	/**
 	 * Generates the shop image in a design similar to the in-game design.
