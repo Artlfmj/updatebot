65ae2f8958e262aafc34875d156d5a1df5b59bcf
Artlfmj <55346663+Artforlfmj@users.noreply.github.com>
2021-10-21 09:40:31 +0200

    Create Slash Manager for Stats

---
+++ slash/stats.js
@@ -0,0 +1,13 @@
+const guild_settings = require('../schemes/guild_settings')
+const Settings = require('../schemes/guild_settings')
+const Discord = require('discord.js')
+const ee = require('../botconfig/embed.json')
+module.exports = {
+	/**
+	 * Generates the shop image in a design similar to the in-game design.
+	 */
+	async slash(client, interaction) {
+      
+    }
+
+}
