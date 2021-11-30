972e7cbeb7d73fa143f021030b0df23786266255
Artlfmj <arthurdepenanster@gmail.com>
2021-11-30 17:36:38 +0100

    Add Handlers

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,47 @@
 //here the event starts
 const config = require("../../botconfig/config.json")
-module.exports = client => {
+const Discord  = require("discord.js");
+const chalk = require("chalk");
+const fs = require("fs");
+module.exports = async(client) => {
+  client.commands = new Discord.Collection();
+  let avail = []
+  client.handlers = new Discord.Collection();
+  client.buttons = new Discord.Collection();
+  client.menus = new Discord.Collection();
+  const jsondir = "slash-json";
+  let cmds = await client.application.commands.fetch()
+  cmds.forEach(async(slashcont) => {
+    avail.push(slashcont.name)
+  })
+  for (const fileName of fs.readdirSync(jsondir)) {
+    const fileContent = require(`../../${jsondir}/${fileName}`);
+    client.commands.set(fileName.split(".")[0], fileContent);
+    console.log(chalk.bold.green(`Loaded command ${fileName}`));
+    if (avail.find(l => l === fileName.replace('.js', "")))
+      console.log(
+        chalk.bold.red(`Command ${fileContent.name} already exists!`)
+      );
+    else client.application.commands.create(fileContent).catch(console.error).then(da => {console.log(chalk.green.bold(`Registered ${da.name} | ${da.id}`))})
+  }
+  const cmdDir = "slash";
+  for (const fileName of fs.readdirSync(cmdDir)) {
+    const fileContent = require(`../../${cmdDir}/${fileName}`);
+    client.handlers.set(fileName.split(".")[0], fileContent);
+    console.log(chalk.bold.green(`Loaded handler ${fileName}`));
+  }
+  const butDir = "buttons";
+  for (const fileName of fs.readdirSync(butDir)) {
+    const fileContent = require(`../../${butDir}/${fileName}`);
+    client.buttons.set(fileName.split(".")[0], fileContent);
+    console.log(chalk.bold.green(`Loaded button ${fileName}`));
+  }
+  const menDir = "menus";
+  for (const fileName of fs.readdirSync(menDir)) {
+    const fileContent = require(`../../${menDir}/${fileName}`);
+    client.menus.set(fileName.split(".")[0], fileContent);
+    console.log(chalk.bold.green(`Loaded menu ${fileName}`));
+  }
   try{
     const stringlength = 69;
     console.log("\n")
--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,43 +1,47 @@
 /**
-  * @INFO
-  * Loading all needed File Information Parameters
-*/
+ * @INFO
+ * Loading all needed File Information Parameters
+ */
 const config = require("../../botconfig/config.json"); //loading config file with token and prefix, and settings
 const ee = require("../../botconfig/embed.json"); //Loading all embed settings like color footertext and icon ...
 const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const chalk = require("chalk"); //this is a library to add colors to text in the console
 
 //here the event starts
 module.exports = async (client, interaction) => {
-   console.log(interaction)
-    if(interaction.commandId === "859467435437850625"){
-        
-        const { prefix } = require('../..//slash/prefix')
-        await prefix(client, interaction)
-    } else if(interaction.commandId === "860477440953810985"){
-        const news = require('../../slash/news')
-        await news.news(client, interaction)
-    } else if(interaction.commandId === "860477439212912671"){
-        const { shop } = require('../../slash/shop')
-        await shop(client, interaction)
-    } else if(interaction.commandId === "871781969795641375"){
-        const { addFunction } = require('../../slash/add-function')
-        await addFunction(client, interaction)
-    } else if(interaction.commandId === "878565228982468649"){
-        const slash = require('../../slash/unsuscribe')
-        await slash.slash(client, interaction)
-    } else if(interaction.commandId === "879386064777805885"){
-        const slash = require('../../slash/aes')
-        await slash.slash(client, interaction)
-    } else if(interaction.commandId === "880805856281763920"){
-        const slash = require('../../slash/upcoming')
-        await slash.slash(client, interaction)
-    } else if(interaction.commandId === "880853624459239505"){
-        const slash = require('../../slash/tab')
-        await slash.slash(client, interaction)
-    } else if(interaction.commandId === "900762986875060235"){
-        const slash = require('../../slash/stats')
-        await slash.slash(client, interaction)
-    }else {
-        return interaction.reply({content : "An error occured", ephemeral : true})
+  console.log(chalk.blue("[EVENT]") + " " + chalk.yellow("interactionCreate"));
+  let translations = await client.getTranslations(interaction.guildId)
+  if (interaction.isCommand()) {
+    const cmd = await client.handlers.get(interaction.commandName);
+    if (cmd) {
+      await cmd.run(client, interaction, translations);
+      console.log(
+        chalk.gray(
+          `Executed command ${interaction.commandName} | ${interaction.guildId} | ${interaction.user.id}`
+        )
+      );
+    } else {
+      interaction.reply({ content: "Command not found!" });
     }
-}
+  }
+  else if(interaction.isButton()) {
+    const cmd = await client.buttons.get(interaction.customId);
+    if (cmd) {
+      await cmd.run(client, interaction, translations);
+      console.log(chalk.gray(`Executed button ${interaction.customId} | ${interaction.guildId || 'DM'} | ${interaction.user.id}`));
+    } else {
+      interaction.reply({ content: "Button not found!" });
+    }
+  }
+  else if(interaction.isSelectMenu()) {
+    
+    const cmd = await client.menus.get(interaction.customId);
+    if (cmd) {
+      await cmd.run(client, interaction, translations);
+      console.log(chalk.gray(`Executed menu ${interaction.customId} | ${interaction.guildId || 'DM'} | ${interaction.user.id}`));
+    } else {
+      interaction.reply({ content: "Select Menu not found!" });
+    }
+  }
+  else interaction.reply({ content: "An error occured", ephemeral: true });
+};
